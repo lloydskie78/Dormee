806d07f91800e8a919d87cc52aeb3942f8c3a27e
celinezosa <zosa.celinemarie@gmail.com>
2019-03-27 16:51:30 +0800

    modified:   public/css/bootstrap.css 	modified:   resources/views/forms/attributes.blade.php

--- resources/views/forms/attributes.blade.php
+++ resources/views/forms/attributes.blade.php
@@ -1,22 +1,25 @@
-<div class="slidecontainer">
+<div class="slidecontainer indicator">
   <i class="fa 
-fa-trash-o fa-lg"></i>&nbsp;Cleanliness <input type="range" min="1" max="5" name="slider1" value="1" class="slider" id="slider1">
-  <p class="text-dark">Value: <span id="value1" class="text-dark"></span></p>
+  fa-trash-o fa-lg"></i>&nbsp;Cleanliness 
+  <span id="value1" class="indicator text-dark pull-right"></span><input type="range" min="1" max="5" name="slider1" value="1" class="slider mt-3" id="slider1">
+  <p class="text-dark"> </p>
 </div>
-<div class="slidecontainer">
-  <i class="fa fa-unlock-alt fa-lg"></i>&nbsp;Safety <input type="range" min="1" max="5" name="slider2" value="1" class="slider " id="slider2">
-  <p class="text-dark">Value: <span id="value2" class="text-dark"></span></p>
+<div class="slidecontainer indicator">
+  <i class="fa fa-unlock-alt fa-lg"></i>&nbsp;Safety <span id="value2" class="indicator text-dark pull-right"></span> <input type="range" min="1" max="5" name="slider2" value="1" class="slider mt-3" id="slider2">
+  <p class="text-dark"></p>
 </div>
-<div class="slidecontainer">
-  <i class="fa fa-picture-o fa-lg"></i>&nbsp;Ambiance <input type="range" min="1" max="5" name="slider3" value="1" class="slider" id="slider3">
-  <p class="text-dark">Value: <span id="value3" class="text-dark"></span></p>
+<div class="slidecontainer indicator">
+  <i class="fa fa-picture-o fa-lg"></i>&nbsp;Ambiance <span id="value3" class="indicator text-dark pull-right"></span> <input type="range" min="1" max="5" name="slider3" value="1" class="slider mt-3" id="slider3">
+  <p class="text-dark"></p>
 </div>
-<div class="slidecontainer">
-  <i class="fa fa-coffee fa-lg"></i>&nbsp;Facilities <input type="range" min="1" max="5" name="slider4" value="1" class="slider" id="slider4">
-  <p class="text-dark">Value: <span id="value4" class="text-dark"></span></p>
+<div class="slidecontainer indicator">
+  <i class="fa fa-coffee fa-lg"></i>&nbsp;Facilities 
+  <span id="value4" class="indicator text-dark pull-right"></span>
+  <input type="range" min="1" max="5" name="slider4" value="1" class="slider mt-3" id="slider4">
+  <p class="text-dark"> </p>
 </div>
-<div class="slidecontainer">
+<div class="slidecontainer indicator">
   <i class="fa 
-fa-subway fa-lg"></i>&nbsp;Access to Transport <input type="range" min="1" max="5" name="slider5" value="1" class="slider" id="slider5">
-  <p class="text-dark">Value: <span id="value5" class="text-dark"></span></p>
+  fa-subway fa-lg"></i>&nbsp;Access to Transport <span id="value5" class="indicator text-dark pull-right"></span> <input type="range" min="1" max="5" name="slider5" value="1" class="slider mt-3 " id="slider5">
+  <p class="text-dark"></p>
 </div>
