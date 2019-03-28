ca71d79a3b867d4b014c7659cb60d64cd9bfd67c
celinezosa <zosa.celinemarie@gmail.com>
2019-03-28 16:40:25 +0800

    modified:   database/migrations/2020_10_12_7_create_users_table.php 	app/Http/Controllers/PaymentController.php 	app/Http/Controllers/RoomController.php 	app/Http/Controllers/RoomFilterController.php 	app/Http/Controllers/RoomPhotoController.php 	app/Http/Controllers/SubscriptionController.php 	app/Http/Controllers/UserDormReviewReportController.php	app/Http/Controllers/UserReportController.php 	app/Http/Controllers/UserRoommatePreferenceController.php 	app/Payment.php 	app/Room.php 	app/RoomFilter.php 	app/RoomPhoto.php 	app/Subscription.php 	app/UserDormReviewReport.php 	app/UserReport.php 	app/UserRoommatePreference.php 	database/migrations/2019_03_28_063112_create_rooms_table.php 	database/migrations/2019_03_28_070952_create_room_filters_table.php 	database/migrations/2019_03_28_072512_create_user_dorm_review_reports_table.php 	database/migrations/2019_03_28_072553_create_user_reports_table.php 	database/migrations/2019_03_28_072814_create_subscriptions_table.php 	database/migrations/2019_03_28_081840_create_user_roommate_preferences_table.php 	database/migrations/2019_03_28_083321_create_room_photos_table.php 	database/migrations/2029_03_13_18_create_user_dorm_bookmarks_table.php

--- database/migrations/2020_10_12_7_create_users_table.php
+++ database/migrations/2020_10_12_7_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->foreign('user_type_id')->references('id')->on('user_types');
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->tinyInteger('email_confirmed')->default(0);
             $table->string('password');
             $table->string('first_name');
             $table->string('last_name');
